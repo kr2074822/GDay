a1bf72100f3467a9e0d3964f360668b70cae89e1
hy <hy@DESKTOP-H3MEHS6>
2021-02-23 14:45:56 +0900

    클래스 등록 페이지 생성 + 등록 페이지 이동 컨트롤러

--- GDay/src/main/java/team/project/gday/member/bmem/controller/BmemController.java
+++ GDay/src/main/java/team/project/gday/member/bmem/controller/BmemController.java
@@ -1,5 +1,15 @@
 package team.project.gday.member.bmem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bMember/*")
 public class BmemController {
 
+	//클래스 신청 화면 이동
+	@RequestMapping("gClassInsert")
+	public String gClassInsert() {
+		return "gClass/gClassInsert";
+	}
 }
