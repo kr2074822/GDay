a2b1f2a49f1e013b7200290dbc77cd34886c3a59
Kang2x2 <kang2x2@naver.com>
2021-02-24 00:00:17 +0900

    비즈니스 관련 jsp controller 추가

--- GDay/src/main/java/team/project/gday/member/bmem/controller/BmemController.java
+++ GDay/src/main/java/team/project/gday/member/bmem/controller/BmemController.java
@@ -20,11 +20,42 @@ public class BmemController {
 		return "mypage/bMemPage/bSellList";
 	}
 	
-	//비즈니스 주문조회
+	//비즈니스 주문조회 이동
 	@RequestMapping("bOrderList")
 	public String bOrderList() {
 		return "mypage/bMemPage/bOrderList";
 	}
+	
+	//비즈니스 환불 목록 조회 이동
+	@RequestMapping("bRefundList")
+	public String bRefundList() {
+		return "mypage/bMemPage/bRefundList";
+	}
+	
+	//비즈니스 주문 취소 목록 조회 이동
+	@RequestMapping("bCancleList")
+	public String bCancleList() {
+		return "mypage/bMemPage/bCancleList";
+	}
+	
+	//비즈니스 회원이 등록한 클래스 목록 조회 이동
+	@RequestMapping("bClassList")
+	public String bClassList() {
+		return "mypage/bMemPage/bClassList";
+	}
+	
+	//비즈니스 수강 신청 목록 조회
+	@RequestMapping("bEnrolmentList")
+	public String bEnrolmentList() {
+		return "mypage/bMemPage/bEnrolmentList";
+	}
+	
+	//비즈니스 수강 취소 목록 조회
+	@RequestMapping("bClassCancleList")
+	public String bClassCancleList() {
+		return "mypage/bMemPage/bClassCancleList";
+	}
+	
 	
 	
 	//비즈니스 내 정보 수정 이동
---
+++ GDay/src/main/webapp/WEB-INF/views/mypage/bMemPage/bClassList.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	내가 등록한 클래스 목록 조회
+</body>
+</html>
