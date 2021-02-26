7ac6728c61d1a208e90de83a92be801f41daad85
Kang2x2 <kang2x2@naver.com>
2021-02-26 18:08:09 +0900

    상품관련 vo/mapper.xml 추가

---
+++ GDay/src/main/java/team/project/gday/Product/model/vo/ProductCTag.java
@@ -0,0 +1,59 @@
+package team.project.gday.Product.model.vo;
+
+// 상품별 해시태그
+public class ProductCTag {
+	private int prdtNo; // 상품 번호
+	private String tagName; // 태그 이름
+	private int ctNo; // 카테고리 번호
+	private String ctName; // 카테고리 명
+	private int tagNo; // 태그 번호
+	
+	public ProductCTag() { }
+
+	public int getPrdtNo() {
+		return prdtNo;
+	}
+
+	public void setPrdtNo(int prdtNo) {
+		this.prdtNo = prdtNo;
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	public int getCtNo() {
+		return ctNo;
+	}
+
+	public void setCtNo(int ctNo) {
+		this.ctNo = ctNo;
+	}
+
+	public String getCtName() {
+		return ctName;
+	}
+
+	public void setCtName(String ctName) {
+		this.ctName = ctName;
+	}
+
+	public int getTagNo() {
+		return tagNo;
+	}
+
+	public void setTagNo(int tagNo) {
+		this.tagNo = tagNo;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductCTag [prdtNo=" + prdtNo + ", tagName=" + tagName + ", ctNo=" + ctNo + ", ctName=" + ctName
+				+ ", tagNo=" + tagNo + "]";
+	}
+	
+}
