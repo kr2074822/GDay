74d0c7c92ba31a5b539d634122c0399a22304939
YJ <YJ@DESKTOP-8JUO3TE>
2021-03-03 11:26:30 +0900

    [YJ] 선물 등록 50%완성

--- GDay/src/main/java/team/project/gday/gift/controller/GiftCtrl.java
+++ GDay/src/main/java/team/project/gday/gift/controller/GiftCtrl.java
@@ -1,11 +1,22 @@
 package team.project.gday.gift.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
+import team.project.gday.Product.model.vo.Gift;
 import team.project.gday.gift.model.service.GiftService;
+import team.project.gday.member.model.vo.Member;
 
 @Controller 
 @SessionAttributes({"loginMember"})
@@ -29,7 +40,40 @@ public class GiftCtrl {
 	
 	// 선물 등록 기능 구현
 	@RequestMapping("insert")
-	public String giftInsert() {
+	public String giftInsert(@ModelAttribute Gift gift, 
+			@ModelAttribute("loginMember") Member loginMember,
+			@RequestParam("hashNo") List<String> hashNo, 
+			@RequestParam(value = "images", required = false) List<MultipartFile> images,
+			HttpServletRequest request) {
+		
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("hashNo", hashNo);
+		map.put("memberNo", loginMember.getMemberNo());
+		map.put("prdtName", gift.getPrdtName());
+		map.put("prdtContent", gift.getPrdtContent());
+		map.put("prdtPrice", gift.getPrdtPrice());
+		System.out.println(gift);
+		System.out.println(hashNo);
+		
+		
+		// 파일 업로드 확인 
+		for (int i = 0; i < images.size(); i++) {
+			System.out.println("images[" + i + "] :" + images.get(i).getOriginalFilename());
+		}
+
+		// 파일 저장 경로설정
+		// HttpServletRequest 객체가 있어야지만 파일 저장 경로를 얻어올 수 있음
+		// -> HttpServletRequest객체는 Controller에서만 사용 가능
+		String savePath = request.getSession().getServletContext().getRealPath("resources/images/thumbnailImg");
+
+		// 게시글 map, 이미지 images, 저장경로 savePath
+
+		// 게시글 삽입 Service 호출
+		int result = service.insertGift(map, images, savePath);
+		
+		
+		
 		System.out.println(1);
 		return "redirect:/";
 	}
--- GDay/src/main/java/team/project/gday/gift/model/dao/GiftDAO.java
+++ GDay/src/main/java/team/project/gday/gift/model/dao/GiftDAO.java
@@ -1,14 +1,59 @@
 package team.project.gday.gift.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import team.project.gday.Product.model.vo.Attachment;
 
 @Repository
 public class GiftDAO {
 
 	@Autowired
 	private SqlSession sqlSession;
+	
+	
+	/** 다음 상품 번호 조회
+	 * @return
+	 */
+	public int selectNextNo() {
+		return sqlSession.selectOne("giftMapper.selectNextNo");
+	}
+	
+	/** 상품 테이블에 삽입
+	 * @param map
+	 * @return
+	 */
+	public int insertProduct(Map<String, Object> map) {
+		return sqlSession.insert("giftMapper.insertProduct", map);
+	}
+
+	/** 선물 테이블에 삽입
+	 * @param map
+	 * @return
+	 */
+	public int insertGift(Map<String, Object> map) {
+		return sqlSession.insert("giftMapper.insertGift", map);
+	}
+
+	/** 선물 태그 테이블에 삽입
+	 * @param map
+	 * @return
+	 */
+	public int insertTag(Map<String, Object> map) {
+		return sqlSession.insert("giftMapper.insertTag", map);
+	}
+	
+	/** 파일 정보 삽입
+	 * @param uploadImages
+	 * @return
+	 */
+	public int insertAttachmentList(List<Attachment> uploadImages) {
+		return sqlSession.insert("giftMapper.insertAttachmentList");
+	}
 	
 	
 	
