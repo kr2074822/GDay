fcd36ddcbdaf0f0040b8eace5405a7e86c049576
hy <hy@DESKTOP-H3MEHS6>
2021-03-05 17:45:12 +0900

    클래스 상세보기 화면 + 백엔드 중간 세이브

--- GDay/src/main/java/team/project/gday/gClass/model/service/GClassService.java
+++ GDay/src/main/java/team/project/gday/gClass/model/service/GClassService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import team.project.gday.Product.model.vo.Attachment;
 import team.project.gday.Product.model.vo.GClass;
 import team.project.gday.member.bmem.model.vo.PageInfo10;
+import team.project.gday.member.model.vo.Member;
 
 public interface GClassService {
  
@@ -26,8 +27,28 @@ public interface GClassService {
 	 */
 	List<Attachment> selectThumbnailList(List<GClass> gCList);
 
+	/**클래스 상세 조회 Service
+	 * @param prdtNo
+	 * @return gClass
+	 */
 	GClass selectGClass(int prdtNo);
 
+	/**클래스 상세 페이지에 포함된 이미지 목록 조회 Service
+	 * @param prdtNo
+	 * @return attachmentList
+	 */
 	List<Attachment> selectAttachmentList(int prdtNo);
+
+	/**클래스 상세 페이지의 판매자 정보 가져오기 Service
+	 * @param memNo
+	 * @return member
+	 */
+	Member selectMember(int memNo);
+
+	/**클래스 상세 페이지의 썸네일 조회 Service
+	 * @param prdtNo
+	 * @return attachment
+	 */
+	Attachment selectThumbnail(int prdtNo);
 
 }
